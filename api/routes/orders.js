cf0ea5241233d1ba5c993b641942f35ecd9eaa1f
mindsworth <princegoziem@gmail.com>
2018-06-25 00:59:19 +0100

    Order Commit

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,24 +1,62 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Order = require('../models/order');
 
 router.get('/', (req, res, next) => {
-	res.status(200).json({
-		message: 'Orders were fetched.',
-	});
+	Order.find()
+		.select('product quantity _id')
+		.exec()
+		.then(result => {
+			res.status(200).json({
+				'Total Orders': result.length,
+				Orders: result,
+			});
+		})
+		.catch(error => {
+			res.status(500).json({ error });
+		});
 });
 
 router.post('/', (req, res, next) => {
-	res.status(201).json({
-		message: 'Order created.',
+	const order = new Order({
+		_id: mongoose.Types.ObjectId(),
+		product: req.body.productId,
+		quantity: req.body.quantity,
 	});
+
+	order
+		.save()
+		.then(result => {
+			res.status(200).json({
+				message: 'Order created.',
+				Order: {
+					_id: result._id,
+					productId: result.productId,
+					quantity: result.quantity,
+				},
+			});
+		})
+		.catch(error => {
+			res.status(500).json({ error });
+		});
 });
 
 router.get('/:orderId', (req, res, next) => {
 	const id = req.params.orderId;
-	res.status(200).json({
-		message: 'Your order is special.',
-		id: id,
-	});
+
+	Order.findById({ _id: id })
+		.select('product quantity _id')
+		.exec()
+		.then(result => {
+			res.status(200).json({
+				message: 'Your order',
+				Order: result,
+			});
+		})
+		.catch(error => {
+			res.status(200).json(error);
+		});
 });
 
 router.delete('/:orderId', (req, res, next) => {
